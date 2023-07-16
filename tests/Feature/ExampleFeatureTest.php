bb1d49e34303bc7f06e75e74be53b497207dc759
Mexion <dev@mexion.com>
2023-07-16 12:41:33 +0200

    auto tests

---
+++ tests/Feature/ExampleFeatureTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Yormy\Xid\Tests\Features;
+
+use Yormy\Xid\Tests\TestCase;
+
+class ExampleFeatureTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
